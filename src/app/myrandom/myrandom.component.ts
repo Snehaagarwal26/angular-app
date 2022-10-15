8d8c951a336ba6670975cf6595f110d0d169183a
123vaibhav-123 <vaibhav_202000449@smit.smu.edu.in>
2022-10-15 11:06:18 +0530

    second commity

---
+++ src/app/myrandom/myrandom.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myrandom',
+  templateUrl: './myrandom.component.html',
+  styleUrls: ['./myrandom.component.css']
+})
+export class MyrandomComponent implements OnInit {
+  randomno: number=0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  generateRandomRollNo=() =>{
+    this.randomno= Math.ceil(Math.random() * 69)
+  }
+
+}
